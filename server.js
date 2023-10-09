7320bb5c3bb98d2f4cae5d0719a06482ea0b465a
Saisuraj Jindam <sjindam@uncc.edu>
2023-10-09 18:51:53 -0400

    Added Backend server to retrive budget data

---
+++ server.js
@@ -0,0 +1,21 @@
+//Budget API 
+
+const express = require('express')
+const cors = require('cors')
+const budget = require("./data.json");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.get("/hello", (req,res)=>{
+    res.send("Hello World.");
+})
+
+app.get("/budget", (req,res)=>{
+    res.json(budget);
+})
+
+app.listen(port, ()=>{
+    console.log("Api Served at http://localhost:"+port);
+})
